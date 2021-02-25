aabc64a81012359b83270cce1e43e929e28cc10b
Merhar <zidmerhar@gmail.com>
2021-02-25 19:19:23 +0700

    test
    test

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    //private Touch touch;
+    Rigidbody rb;
+    [SerializeField] float speed = default;
+    int score = 0;
+    public Text scoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        scoreText.text = score.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float hor = Input.GetAxis("Horizontal");
+
+        Vector3 move = new Vector3(hor * 6f, 0, speed);
+        rb.AddForce(move);
+
+        /*   if(Input.touchCount > 0)
+           {
+               touch = Input.GetTouch(0);
+
+               if(touch.phase == TouchPhase.Moved)
+               {
+                   transform.position = new Vector3(transform.position.x + touch.deltaPosition.x * 0.01f, transform.position.y, transform.position.z);
+               }
+           }
+        */
+
+        
+
+    }
+
+    private void FixedUpdate()
+    {
+        Quaternion rotationX = Quaternion.AngleAxis(10, Vector3.right);
+        transform.rotation *= rotationX;
+    }
+
+
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag("small"))
+        {
+            score += 5;
+
+            col.gameObject.AddComponent<FixedJoint>();
+            col.gameObject.GetComponent<FixedJoint>().connectedBody = rb;
+            col.gameObject.GetComponent<Collider>().isTrigger = true;
+            col.gameObject.GetComponent<Rigidbody>().useGravity = false;
+            col.gameObject.GetComponent<Rigidbody>().mass = 0;
+
+            transform.localScale = new Vector3(transform.localScale.x + 0.2f, transform.localScale.y + 0.2f, transform.localScale.z + 0.2f);
+
+            scoreText.text = score.ToString();
+
+            speed += 0.2f;
+        }
+
+
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("human"))
+        {
+
+            score += 15;
+
+            transform.localScale = new Vector3(transform.localScale.x + 0.2f, transform.localScale.y + 0.2f, transform.localScale.z + 0.2f);
+
+            scoreText.text = score.ToString();
+        }
+    }
+
+
+
+}
---
+++ Assets/Scripts/RegdollController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RegdollController : MonoBehaviour
+{
+
+    public Animator animator;
+    public Rigidbody[] allRb;
+    private Rigidbody mainRb;
+
+    private void Awake()
+    {
+        for (int i = 0; i < allRb.Length; i++)
+        {
+            allRb[i].isKinematic = true;
+            allRb[i].gameObject.AddComponent<FixedJoint>();
+        }
+    }
+
+    private void Start()
+    {
+        mainRb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        mainRb.AddForce(Vector3.forward * 2f);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            animator.enabled = false;
+            gameObject.GetComponent<Collider>().enabled = false;
+            
+            for (int i = 0; i < allRb.Length; i++)
+            {
+                allRb[i].isKinematic = false;
+                allRb[i].gameObject.GetComponent<FixedJoint>().connectedBody = other.gameObject.GetComponent<Rigidbody>();
+                allRb[i].gameObject.GetComponent<Collider>().isTrigger = true;
+                allRb[i].gameObject.GetComponent<Rigidbody>().mass = 0;
+                allRb[i].gameObject.GetComponent<Rigidbody>().useGravity = false;
+            }
+
+        }
+    }
+}
